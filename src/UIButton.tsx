7cdb994b591e6a3405b5e848550f58de4400c5b2
Pablo Adoue Peralta <pablo.adoue@me.com>
2022-07-25 07:56:42 -0500

    chore: uibutton-component
    Added the definition for the UIButton component on the library exports

---
+++ src/UIButton.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Button, IconButton } from 'native-base';
+
+import type { TUIIconName } from './UIIcon';
+import { UIIcon } from './UIIcon';
+
+export function UIButton(props: TUIButtonProps) {
+    if (typeof props.label === 'string') {
+        return (
+            <Button
+                variant={props.variant}
+                isLoading={props.loading}
+                isDisabled={props.disabled}
+                colorScheme={props.color}
+                size={props.size}
+                leftIcon={
+                    props.leftIcon && (
+                        <UIIcon name={props.leftIcon} size={'sm'} />
+                    )
+                }
+                rightIcon={
+                    props.rightIcon && (
+                        <UIIcon name={props.rightIcon} size={'sm'} />
+                    )
+                }
+                onPress={() => {
+                    if (typeof props.onPress === 'function') {
+                        props.onPress();
+                    }
+                }}
+            >
+                {props.label}
+            </Button>
+        );
+    } else if (typeof props.icon === 'string') {
+        return (
+            <IconButton
+                variant={props.variant}
+                isDisabled={props.disabled}
+                colorScheme={props.color}
+                size={props.size}
+                icon={<UIIcon name={props.icon} size={'sm'} />}
+                onPress={() => {
+                    if (typeof props.onPress === 'function') {
+                        props.onPress();
+                    }
+                }}
+            />
+        );
+    }
+
+    return null;
+}
+
+UIButton.defaultProps = {
+    variant: 'ghost',
+    color: 'primary',
+    disabled: false,
+    size: 'lg',
+};
+
+export type TUIButtonProps = {
+    label?: string;
+    size: 'sm' | 'md' | 'lg';
+    icon?: TUIIconName;
+    leftIcon?: TUIIconName;
+    rightIcon?: TUIIconName;
+    loading?: boolean;
+    disabled?: boolean;
+    onPress?: () => void;
+    color?: string;
+    variant?: 'solid' | 'subtle' | 'link' | 'outline' | 'ghost';
+};
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import {
     TRenaissanceProviderPallete,
     TRenaissanceProviderProps,
 } from './RenaissanceProvider';
+import { TUIButtonProps, UIButton } from './UIButton';
 import { TUIIconName, TUIIconProps, UIIcon } from './UIIcon';
 
-export { UIIcon, RenaissanceProvider };
+export { UIButton, UIIcon, RenaissanceProvider };
 
 export type {
     TRenaissanceProviderProps,
     TRenaissanceProviderPallete,
     TUIIconName,
     TUIIconProps,
+    TUIButtonProps,
 };
