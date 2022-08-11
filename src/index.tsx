ea30330a6acc256ff179824454a138bf53a5cb79
Pablo Adoue Peralta <pablo.adoue@me.com>
2022-08-11 10:22:28 -0500

    feat: new-components
    Incorporated new components to the export:
    
    - UIBox
    - UIAlertDialogue
    - UIIcon

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,25 @@ import {
     TRenaissanceProviderPallete,
     TRenaissanceProviderProps,
 } from './RenaissanceProvider';
+import { TUIAlertDialogProps, UIAlertDialog } from './UIAlertDialog';
+import { TUIBoxProps, UIBox } from './UIBox';
 import { TUIButtonProps, UIButton } from './UIButton';
+import { TUIIconName, TUIIconProps, UIIcon } from './UIIcon';
 
-export { AppErrorContext, RenaissanceProvider, UIButton };
+export {
+    AppErrorContext,
+    RenaissanceProvider,
+    UIButton,
+    UIBox,
+    UIAlertDialog,
+    UIIcon,
+};
 export type {
     TRenaissanceProviderPallete,
     TRenaissanceProviderProps,
     TUIButtonProps,
+    TUIBoxProps,
+    TUIAlertDialogProps,
+    TUIIconName,
+    TUIIconProps,
 };
