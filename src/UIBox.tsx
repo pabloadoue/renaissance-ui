3a43311dd25433d6cd7e2ffcb80c8d9f1ac4c27e
Pablo Adoue Peralta <pablo.adoue@me.com>
2022-08-11 11:45:56 -0500

    feat: uibox-changes
    Added different props to the UIBox to make it more customizable

--- src/UIBox.tsx
+++ src/UIBox.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { View } from 'native-base';
+import { useColorMode, View } from 'native-base';
 
 export const UIBox = (props: TUIBoxProps) => {
+    const { colorMode } = useColorMode();
     return (
-        <View bg={{ _dark: 'gray5.500', _light: 'white' }} borderRadius={8}>
+        <View
+            bg={colorMode === 'dark' ? 'gray5.500' : 'white'}
+            borderRadius={props.borderRadius}
+            padding={props.padding}
+        >
             {props.children}
         </View>
     );
 };
 
+UIBox.defaultProps = {
+    borderRadius: 8,
+    padding: 4,
+};
+
 export type TUIBoxProps = {
     children: JSX.Element | JSX.Element[];
+    padding?: number;
+    borderRadius?: number;
 };
