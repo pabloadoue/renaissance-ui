620a6abc26092b1ca4890554b51c7be9b90818da
Pablo Adoue Peralta <pablo.adoue@me.com>
2022-08-11 07:31:50 -0500

    feat: example-changes
    Added changes to the example package

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { multiply } from 'renaissance-ui';
 
 export default function App() {
-    const [result, setResult] = React.useState<number | undefined>();
-
-    React.useEffect(() => {
-        multiply(3, 7).then(setResult);
-    }, []);
+    const [result] = React.useState<number | undefined>();
 
     return (
         <View style={styles.container}>
